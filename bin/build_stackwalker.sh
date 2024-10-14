2598995857502b0531087fc80cae2442677b308c
Daniel Thorn <dthorn@mozilla.com>
2024-10-14 13:21:16 -0700

    bug-1924072: update stackwalker to 0.22.2 (#16)

--- bin/build_stackwalker.sh
+++ bin/build_stackwalker.sh
@@ -14,8 +14,8 @@
 set -euo pipefail
 
 # From: https://github.com/rust-minidump/rust-minidump
-MINIDUMPREV=v0.21.1
-MINIDUMPREVDATE=2024-03-01
+MINIDUMPREV=v0.22.2
+MINIDUMPREVDATE=2024-10-10
 
 TARFILE="socorro-stackwalker.${MINIDUMPREVDATE}.${MINIDUMPREV}.tar.gz"
 
